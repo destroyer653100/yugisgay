f3fd18a70356c6b2bc474a74edb810e5718cea37
Name=Yug <112211279+YUG38@users.noreply.github.com>
2024-02-16 11:50:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,20 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: { // You can add multiple users by doing username: 'password'. 
-    interstellar: 'password',
-  },
+  challenge: true, // Set to true if you want to enable password protection.
+  users: {
+		Yug: "YUG69",
+		Blox: "BloxxLoxx",
+		Hibbrya79: "704279",
+		emumaster: "Vc55$Te78+W9",
+		NighthawkValga: "tgValga",
+		motercycles: "themonkeyintree",
+		Trey: "Jujutsu",
+		Sahi: "chromebook",
+		Kaylem: "Among us",
+		TomDom83: "Tom12345",
+		hunter: "Dixie696!",
+		Komasanzzz: "Puff",
+    FS_BoxTops: "BoxT0ps*",
+	},
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
 }
