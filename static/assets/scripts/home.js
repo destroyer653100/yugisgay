f82ad8f8053548f10349d022d1251724b7384fc4
Name=Yug <112211279+YUG38@users.noreply.github.com>
2024-02-16 11:49:35 -0600

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -15,8 +15,8 @@ if (!inFrame && !navigator.userAgent.includes('Firefox')) {
     const style = iframe.style
     const link = doc.createElement('link')
 
-    const name = localStorage.getItem('name') || 'My Drive - Google Drive'
-    const icon = localStorage.getItem('icon') || 'https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png'
+    const name = localStorage.getItem('name') || 'Home | Schoology'
+    const icon = localStorage.getItem('icon') || 'https://schoology.sdmfschools.org/sites/all/themes/schoology_theme/favicon.ico'
 
     doc.title = name
     link.rel = 'icon'
